54f476815d2bfb6fe4f4f5610907480f4d1fb966
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-10-30 20:22:29 +0200

    kalkulators

--- vertibu_nolasisana.js
+++ vertibu_nolasisana.js
@@ -2,5 +2,16 @@ count = ()=>{
     let val1 = +document.querySelector("#inp1").value,
         val2 = +document.querySelector("#inp2").value;
 
-        document.querySelector("#result").value = val1+val2
+        
+}
+
+
+let saskaitisana  = (a,b)=>{return a+b};
+let reizinashana  = (a,b)=>{return a*b};
+let dalishana  = (a,b)=>{return a/b};
+let atnemshana  = (a,b)=>{return a-b};
+
+
+let showResult = (result)=>{
+    document.querySelector("#result").value = result;
 }
