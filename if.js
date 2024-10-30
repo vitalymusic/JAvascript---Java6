4dcc514fb1206c1836541999f4180e1c7c2996e6
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-10-30 20:09:22 +0200

    switch

---
+++ if.js
@@ -0,0 +1,50 @@
+// IF
+let a = 9,b=4;
+
+let teksts = "10"; 
+
+if(a==10 && b==5){
+    document.write("ok");
+}else{
+    document.write("not ok");
+}
+
+
+if(a>=10){
+    document.write("Skatlis ir lielāks par 10");
+}else if(a>5 && a<10){
+    document.write("Skatlis ir lielāks par 5 bet mazāks par 10");
+}else if(a<5 && a>0){
+    document.write("Skatlis ir mazāks par 5 bet lielāks par 0");
+}else{
+    document.write("Skaitlis ir 0 vai mazāks par 0");
+}
+
+
+// Switch Case
+
+
+let dienasNumurs = 4;
+let dienasNosaukums = "";
+
+switch(dienasNumurs){
+    case 1: dienasNosaukums = "Pirmdiena";break;
+    case 2: dienasNosaukums = "Otrdiena";break;
+    case 3: dienasNosaukums = "Trešdiena";break;
+    default:dienasNosaukums = "Tādas dienas nav";break;
+
+}
+
+document.write(dienasNosaukums);
+
+
+
+
+
+
+
+
+
+// if(a==10 || b==5){
+//     document.write("ok");
+// }
