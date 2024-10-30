bcf208dd158e03a4e316ad8f610b21c73d50b59b
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-10-30 19:21:21 +0200

    vertibu

---
+++ vertibu_nolasisana.js
@@ -0,0 +1,6 @@
+count = ()=>{
+    let val1 = +document.querySelector("#inp1").value,
+        val2 = +document.querySelector("#inp2").value;
+
+        document.querySelector("#result").value = val1+val2
+}
