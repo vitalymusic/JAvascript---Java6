6411e693e77e8acb0319412092bb6653dee082f2
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-10-30 20:40:35 +0200

    calc last

--- vertibu_nolasisana.js
+++ vertibu_nolasisana.js
@@ -4,7 +4,22 @@ count = ()=>{
         darbiba = document.querySelector("#inp3").value;
 
         
-        
+        if(darbiba=="+"){
+            showResult(saskaitisana(val1,val2));
+        }else if(darbiba=="-"){
+            showResult(atnemshana(val1,val2));
+        }else if(darbiba=="*"){
+            showResult(reizinashana(val1,val2));
+        }else if(darbiba=="/"){
+            if(val2==0){
+                return alert("Uz 0 dalīt nedrīkst!!!");
+            }
+            showResult(dalishana(val1,val2));
+
+        }else{
+            showResult("Nav izvēlēta neviena darbība!");
+        }
+
 
 
 
